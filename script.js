6c5fb34669123fb64c64332c08d8c42697b1d79e
andrea-scm <a.scimitto@gmail.com>
2019-05-14 20:59:39 +0200

    milestone 1:avoided to send message with no text on input

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 $('.fa-paper-plane').click(function () {
   var inputMessage = $('.message-to-send').val();
-  $('.messages').append(
-  '<div class="message-container">'+
-    '<div class="message utente">'+
-    inputMessage +
-    '</div>'+
-  '</div>');
-  $('.message-to-send').val('');
-  //imposta lo scroll alla fine del div in modo da visualizare subito il msg inviato
-  $('.messages').scrollTop($('.messages').height());
+  if (inputMessage != '') {
+    $('.messages').append(
+    '<div class="message-container">'+
+      '<div class="message utente">'+
+      inputMessage +
+      '</div>'+
+    '</div>');
+    $('.message-to-send').val('');
+    //imposta lo scroll alla fine del div in modo da visualizare subito il msg inviato
+    $('.messages').scrollTop($('.messages').height());
+  }
 });
 
 
@@ -18,7 +20,7 @@ $('.message-to-send').keypress(function (enter) {
   il quale viene riconosciuto dalla funzione .which*/
   var keyCode = (enter.which);
   //se il tasto premuto è = 13,ovvero Invio,allora mando il messaggio
-  if (keyCode == '13') {
+  if (keyCode == '13' && inputMessage != '') {
     $('.messages').append(
     '<div class="message-container">'+
       '<div class="message utente">'+
