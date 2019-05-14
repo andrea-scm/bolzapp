58f1a501de0989966902926df2df60a1f8aa0894
andrea-scm <a.scimitto@gmail.com>
2019-05-14 20:45:12 +0200

    milestone 1:added keypress function to send message via 'Enter' keyboard button

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ $('.fa-paper-plane').click(function () {
   //imposta lo scroll alla fine del div in modo da visualizare subito il msg inviato
   $('.messages').scrollTop($('.messages').height());
 });
+
+
+$('.message-to-send').keypress(function (enter) {
+  var inputMessage = $('.message-to-send').val();
+  /*salvo in una variabile il codice del tasto premuto
+  il quale viene riconosciuto dalla funzione .which*/
+  var keyCode = (enter.which);
+  //se il tasto premuto è = 13,ovvero Invio,allora mando il messaggio
+  if (keyCode == '13') {
+    $('.messages').append(
+    '<div class="message-container">'+
+      '<div class="message utente">'+
+      inputMessage +
+      '</div>'+
+    '</div>');
+    $('.message-to-send').val('');
+    //imposta lo scroll alla fine del div in modo da visualizare subito il msg inviato
+    $('.messages').scrollTop($('.messages').height());
+  }
+});
